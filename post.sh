3d1151e4f9207d6b2a0340a3bd3e6bc87c42fd78
kinzlong <kinseytlong@gmail.com>
2025-07-01 11:02:18 -0700

    edit

--- post.sh
+++ post.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
 #SBATCH --job-name=alphafold_IDR_puller
-
 #SBATCH --account=fc_moilab
-
 #SBATCH --partition=savio3_htc
-
-#SBATCH --ntasks-per-node=1
-
-#SBATCH --cores=4
-
-#SBATCH --time=20:00:00 --mem 9gb
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=24:00:00
+#SBATCH --mem=9gb
 
 source /global/scratch/projects/fc_moilab/kinseylong/CAM5/CAM5/bin/activate
 python /global/scratch/projects/fc_moilab/kinseylong/CAM5/ArabidopsisIDRpuller.py
